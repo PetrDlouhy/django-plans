2cc6e58a3caa20f44b59a7c94ae9d8258df9e39d
Petr Dlouhý <petr.dlouhy@email.cz>
2019-02-03 00:54:11 +0100

    fix expired plan quota behaviour: Use default free plans quotas or don't validate

--- plans/quota.py
+++ plans/quota.py
@@ -1,6 +1,16 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+
+
 def get_user_quota(user):
     """
-    Tiny helper for getting quota dict for user (left mostly for backward compatibility)
+    Tiny helper for getting quota dict for user
+    If user has expired plan, return def plan or None
     """
+    if user.is_anonymous or user.userplan.is_expired():
+        from .models import Plan
+        default_plan = Plan.get_default_plan()
+        if default_plan is None or not default_plan.is_free():
+            raise ValidationError(_('User plan has expired'))
+        return default_plan.get_quota_dict()
     return user.userplan.plan.get_quota_dict()
-
